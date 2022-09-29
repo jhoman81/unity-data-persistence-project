1241b1c0c291c11f4ad073fed56244b650ee4ac9
jhoman81 <josh.homan@gmail.com>
2022-09-29 14:03:21 -0500

    Added menu scene

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+    public string playerName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
